876f165895c1b0b754e57ffd2211d7c12f0a8adf
tienpham21 <viettienpham.90@gmail.com>
2024-05-15 08:29:18 +0700

    update bai tap

--- LTVTH/hocjs/Baitap-js/Buoi6/bai2/bai2.js
+++ LTVTH/hocjs/Baitap-js/Buoi6/bai2/bai2.js
@@ -19,3 +19,22 @@ arr.forEach(function (item) {
 
 var resultDiv = document.getElementById("result");
 resultDiv.innerHTML = "<p>Flattened Array: " + flattenedArr.join(", ") + "</p>";
+
+/*
+Bài này nên sử dụng đệ quy để có thể xử lý được các mảng lồng nhiều cấp. Có thể tham khảo cách sau:
+var arr = [0, 1, [2, 3], [4, 5, [6, 7]], [8, [9, 10, [11, 12]]]];
+function flatArr(arr) {
+  var newArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    // Nếu phần tử tại vị trí i là 1 mảng thì gọi lại hàm flatArr, ngược lại nếu không phải mảng sẽ thêm phần tử đó vào mảng mới
+    if (Array.isArray(arr[i])) {
+      newArr = newArr.concat(flatArr(arr[i]));
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+console.log(flatArr(arr));
+
+*/
--- LTVTH/hocjs/Baitap-js/Buoi6/bai4/bai4.js
+++ LTVTH/hocjs/Baitap-js/Buoi6/bai4/bai4.js
@@ -1,58 +1,32 @@
 const posts = [
-  {
-    title: "Tiêu đề bài viết 1",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat dolores ea quas possimus non! Corrupti quae nesciunt ipsam suscipit eum quam repellendus dolores harum sit, laudantium delectus quisquam amet atque.",
-    imageUrl: "https://picsum.photos/200/300",
-  },
-  {
-    title: "Tiêu đề bài viết 2",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat dolores ea quas possimus non! Corrupti quae nesciunt ipsam suscipit eum quam repellendus dolores harum sit, laudantium delectus quisquam amet atque.",
-    imageUrl: "https://picsum.photos/200/300",
-  },
-  {
-    title: "Tiêu đề bài viết 3",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat dolores ea quas possimus non! Corrupti quae nesciunt ipsam suscipit eum quam repellendus dolores harum sit, laudantium delectus quisquam amet atque.",
-    imageUrl: "https://picsum.photos/200/300",
-  },
+  [
+    "Tiêu đề bài viết 1",
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat dolores ea quas possimus non! Corrupti quae nesciunt ipsam suscipit eum quam repellendus dolores harum sit, laudantium delectus quisquam amet atque.",
+    "https://picsum.photos/380/230",
+  ],
+  [
+    "Tiêu đề bài viết 2",
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat dolores ea quas possimus non! Corrupti quae nesciunt ipsam suscipit eum quam repellendus dolores harum sit, laudantium delectus quisquam amet atque.",
+    "https://picsum.photos/380/230",
+  ],
+  [
+    "Tiêu đề bài viết 3",
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat dolores ea quas possimus non! Corrupti quae nesciunt ipsam suscipit eum quam repellendus dolores harum sit, laudantium delectus quisquam amet atque.",
+    "https://picsum.photos/380/230",
+  ],
 ];
 
 const postsContainer = document.getElementById("posts-container");
 
-// Bài viết 1
-const post1 = document.createElement("div");
-post1.className = "post";
-post1.innerHTML = `
-  <img src="${posts[0].imageUrl}" alt="${posts[0].title}">
-  <div class="post-content">
-      <div class="post-title">${posts[0].title}</div>
-      <div class="post-description">${posts[0].description}</div>
-  </div>
-`;
-postsContainer.appendChild(post1);
-
-// Bài viết 2
-const post2 = document.createElement("div");
-post2.className = "post reverse";
-post2.innerHTML = `
-  <img src="${posts[1].imageUrl}" alt="${posts[1].title}">
-  <div class="post-content">
-      <div class="post-title">${posts[1].title}</div>
-      <div class="post-description">${posts[1].description}</div>
-  </div>
-`;
-postsContainer.appendChild(post2);
-
-// Bài viết 3
-const post3 = document.createElement("div");
-post3.className = "post";
-post3.innerHTML = `
-  <img src="${posts[2].imageUrl}" alt="${posts[2].title}">
-  <div class="post-content">
-      <div class="post-title">${posts[2].title}</div>
-      <div class="post-description">${posts[2].description}</div>
-  </div>
-`;
-postsContainer.appendChild(post3);
+posts.forEach((post, index) => {
+  const newPost = document.createElement("div");
+  newPost.className = `post${index % 2 === 0 ? "" : " reverse"}`;
+  newPost.innerHTML = `
+    <img src="${post[2]}" alt="${post[0]}">
+    <div class="post-content">
+        <div class="post-title">${post[0]}</div>
+        <div class="post-description">${post[1]}</div>
+    </div>
+  `;
+  postsContainer.appendChild(newPost);
+});
