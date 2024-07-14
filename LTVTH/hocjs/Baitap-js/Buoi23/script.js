33fe122ee2f6560454d451033a3b71d16bedf8f8
tienpham21 <viettienpham.90@gmail.com>
2024-07-14 16:34:06 +0700

    dang hoan thien do

--- LTVTH/hocjs/Baitap-js/Buoi23/script.js
+++ LTVTH/hocjs/Baitap-js/Buoi23/script.js
@@ -2,7 +2,6 @@ const postContainer = document.getElementById("post-container");
 const loading = document.getElementById("loading");
 let page = 1;
 let limit = 5;
-let loadedPostIds = new Set(); // Tập hợp các ID bài viết đã tải
 
 async function fetchPosts(page, limit) {
   const res = await fetch(
@@ -18,23 +17,19 @@ function getRandomPosts(posts) {
 
 function showPosts(posts) {
   posts.forEach((post) => {
-    if (!loadedPostIds.has(post.id)) {
-      loadedPostIds.add(post.id); // Thêm ID bài viết vào tập hợp đã tải
-
-      const postElement = document.createElement("div");
-      postElement.classList.add("post");
-      postElement.innerHTML = `
-                <div class="avatar">${post.title.charAt(0).toUpperCase()}</div>
-                <div class="content">
-                    <h2>${post.title}</h2>
-                    <p class="meta">@${
-                      post.userId
-                    } - ${new Date().toLocaleTimeString()}</p>
-                    <p>${post.body}</p>
-                </div>
-            `;
-      postContainer.appendChild(postElement);
-    }
+    const postElement = document.createElement("div");
+    postElement.classList.add("post");
+    postElement.innerHTML = `
+            <div class="avatar">${post.title.charAt(0).toUpperCase()}</div>
+            <div class="content">
+                <h2>${post.title}</h2>
+                <p class="meta">@${
+                  post.userId
+                } - ${new Date().toLocaleTimeString()}</p>
+                <p>${post.body}</p>
+            </div>
+        `;
+    postContainer.appendChild(postElement);
   });
 }
 
